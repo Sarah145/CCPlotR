24e55a17aff9353631864182110cf747a67c5522
Sarah145 <sarahennis805@hotmail.com>
2023-08-18 09:24:06 +0100

    small fixes

--- R/paletteMartin.R
+++ R/paletteMartin.R
@@ -9,6 +9,6 @@
 #' scales::show_col(paletteMartin(n=9))
 
 paletteMartin <- function(n = 15){
-    cols <- c("#009292FF", "#490092FF", "#6DB6FFFF", "#920000FF", "#FFB6DBFF", "#FFFF6DFF", "#B66DFFFF", "#006DDBFF", "#924900FF", "#FF6DB6FF", "#004949FF", "#DB6D00FF", "#B6DBFFFF", "#24FF24FF", "#000000FF")
-    return(cols[seq_len(n)])
+  cols <- c("#009292FF", "#490092FF", "#6DB6FFFF", "#920000FF", "#FFB6DBFF", "#FFFF6DFF", "#B66DFFFF", "#006DDBFF", "#924900FF", "#FF6DB6FF", "#004949FF", "#DB6D00FF", "#B6DBFFFF", "#24FF24FF", "#000000FF")
+  return(cols[seq_len(n)])
 }
